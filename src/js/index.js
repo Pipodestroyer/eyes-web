969db730cf703945f5d881970c46f43e194bd0d4
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2025-09-28 14:22:13 -0500

    Animacion bien equisde
    El papu y el dog

--- src/js/index.js
+++ src/js/index.js
@@ -6,20 +6,21 @@ function manage(){
         botones.disabled = true;
     } else {
         botones.disabled = false;
-        botones.addEventListener('click', function() {
-            document.body.classList.add('fade-out');
+    }
+}
+
+botones.addEventListener('click', function() { 
+        document.body.classList.add('fade-out');
             document.body.classList.remove('fade-in');
             setTimeout(() =>{
                 window.location.href = "test.html";
         }, 1000);
+    
 });
-    }
-}
 
 definput.addEventListener('input', manage);
 
-botones.disabled=true;
-
 document.addEventListener('DOMContentLoaded', (event) => {
   document.body.classList.add('fade-in');
+    botones.disabled = true;
 });
