49163e17d7a70fdbd19ce5d7219c4c89efc1a08a
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2025-09-28 18:27:52 -0500

    shake it babey

--- src/js/form.js
+++ src/js/form.js
@@ -11,6 +11,21 @@ function manage(){
     }
 }
 
+function isNumber(evt, id){
+    var errorshake = document.getElementById(`${id}`);
+    evt = (evt) ? evt : window.Event;
+    var charCode = (evt.wich) ? evt.wich : evt.keyCode;
+    if(charCode > 31 && (charCode <48 || charCode > 57)){
+        errorshake.classList.add('error');
+         setTimeout(function() {
+                errorshake.classList.remove('error');
+        }, 300);
+        return false;
+    }
+    return true;
+
+}
+
 botones.addEventListener('click', function() { 
         document.body.classList.add('fade-out');
             document.body.classList.remove('fade-in');
