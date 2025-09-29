31b882a9a08129bb2885b4e87a3fc8756580eb32
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2025-09-28 21:50:47 -0500

    Update results.js

--- src/js/results.js
+++ src/js/results.js
@@ -1,6 +1,8 @@
 const botones = document.getElementById('send');
 const resultado = document.getElementsByClassName('resultado')[0];
+const referencia = document.querySelector('h2');
 const miopia = localStorage.getItem('miopia');
+const usuario = localStorage.getItem('username');
 
 botones.addEventListener('click', function() { 
         document.body.classList.add('fade-out');
@@ -15,6 +17,7 @@ botones.addEventListener('click', function() {
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.body.classList.add('fade-in');
+    referencia.innerHTML = `Estimado ${usuario}, los resultados de su prueba ya estan listos.`;
     resultado.innerHTML = `${miopia}`;
     console.log("loaded");
 });
